7068a11930129dce7f1130bd8f171a0028bde6fa
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-07-28 23:31:28 +0530

    Added menu close function on body while menu is open

--- JavaScript/menuBar.js
+++ JavaScript/menuBar.js
@@ -1,7 +1,12 @@
 function openMenu() {
+  document.getElementById("screenBlocker").style.display="block";
+  document.getElementById("body").style.overflowY="hidden";
   document.getElementById("menu-box").style.transform="translate(0)";
 }
 
 function closeMenu() {
+  document.getElementById("screenBlocker").style.display="none";
+  document.getElementById("body").style.overflowY="scroll";
   document.getElementById("menu-box").style.transform="translate(-100%)";
+
 }
