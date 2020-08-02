5898c7a20decb1594915b89d8d0a1ed753551a21
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-08-02 17:29:53 +0530

    Bug Fix to improve speed

--- JavaScript/preloader.js
+++ JavaScript/preloader.js
@@ -1,6 +1,7 @@
 function bodyLoaded() {
-  document.getElementById("preloader").style.width="0";
+  // document.getElementById("preloader").style.width="0";
   document.getElementById("preloader-div").style.width="0";
-  document.getElementById("preloader").style.height="0";
+  document.getElementById("preloader-div").style.borderRadius="100vh";
+  // document.getElementById("preloader").style.height="0";
   document.getElementById("preloader-div").style.height="0";
 }
