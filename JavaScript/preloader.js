dffbe4bfacb4c7bb8cd727791ecf54e026b44e9c
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-08-02 16:46:46 +0530

    Added a preloader to the Index page

---
+++ JavaScript/preloader.js
@@ -0,0 +1,6 @@
+function bodyLoaded() {
+  document.getElementById("preloader").style.width="0";
+  document.getElementById("preloader-div").style.width="0";
+  document.getElementById("preloader").style.height="0";
+  document.getElementById("preloader-div").style.height="0";
+}
