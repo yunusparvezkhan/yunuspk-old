0b6d570a30709b74d26434a6a2c1ef1666411402
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-09-24 22:41:05 +0530

    Completed the menu function in the portfolio page

--- portfolio/functions.js
+++ portfolio/functions.js
@@ -1,3 +1,9 @@
    function openMenu() {
      document.getElementById("menu-Box").style.transform="translate(0%)";
+     document.getElementById("screenblocker").style.display="block";
+   }
+
+   function closeMenu() {
+     document.getElementById("menu-Box").style.transform="translate(-100%)";
+     document.getElementById("screenblocker").style.display="none";
    }
